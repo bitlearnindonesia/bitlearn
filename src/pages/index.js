3a03108c92975b42e62a3324aad8524d120f62f1
Ridwan Satria <73154922+riidwansatria@users.noreply.github.com>
2022-02-02 19:50:00 +0900

    removed unnecessary files

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ const Index = ({ data, location }) => {
 
       <div className="grid grid-cols-1 gap-24">
         {/* CTA */}
-        <div className="mx-auto text-center px-4 pt-36 space-y-12 max-w-4xl">
-          <h2 className="md:text-4xl text-3xl text-center font-bold mb-8">
+        <div className="mx-auto text-center px-4 pt-12 sm:pt-36 space-y-12 max-w-4xl">
+          <h2 className="md:text-4xl text-3xl text-justify sm:text-center font-bold mb-8">
           Kesulitan dalam mengoperasikan Google Education? Hubungi kami untuk mendapatkan bantuan profesional!
           </h2>
           <p className="text-xl">Bitlearn dapat membantu anda untuk mengoperasikan Google Education. Mulai dari membagian materi, menjadwalkan pertemuan kelas, sampai asesmen pencapaian siswa dapat kami urus. Tanpa perlu repot mengurus semua itu, anda tinggal menyusun rencana pembelajaran dan kami yang lakukan sisanya.</p>
@@ -30,7 +30,7 @@ const Index = ({ data, location }) => {
           </div>
           <div className="col-span-4">
             <h2 className="md:text-4xl text-3xl text-center sm:text-left font-bold my-4">Menghemat tenaga dan waktu agar bisa lebih fokus dalam mendidik</h2>
-            <p className="text-lg">Rumitnya proses persiapan pembelajaran pada media daring mengurangi tingkat efektivitas guru dalam menyampaikan ilmunya. Kami dapat mempersingkat waktu dan menghemat tenaga anda agar ilmu dapat langsung disampaikan. Kolaborasi antara guru dan bitlearn akan menghasilkan ekosistem belajar yang sempurna, hemat waktu dan ramah pengguna.</p>
+            <p className="text-lg text-justify sm:text-left">Rumitnya proses persiapan pembelajaran pada media daring mengurangi tingkat efektivitas guru dalam menyampaikan ilmunya. Kami dapat mempersingkat waktu dan menghemat tenaga anda agar ilmu dapat langsung disampaikan. Kolaborasi antara guru dan bitlearn akan menghasilkan ekosistem belajar yang sempurna, hemat waktu dan ramah pengguna.</p>
           </div>
           <div className="col-span-2 items-center">
             <img src={placeholder} className="hidden sm:block w-48 ml-auto" alt="placeholder"/>
@@ -44,7 +44,7 @@ const Index = ({ data, location }) => {
           </div>
           <div className="col-span-4">
             <h2 className="md:text-4xl text-2xl text-center sm:text-left font-bold my-4">Selalu menggunakan teknologi terkini dari Google, mengikuti perkembangan zaman</h2>
-            <p className="text-lg">Tanpa perlu repot memasukkan bahan ajar satu persatu, guru hanya perlu memberikan rencana pembelajaran. Bitlearn akan menyediakan ruang kelasnya. Pengajaran multi kelas menjadi lebih mudah dan tidak repot.</p>
+            <p className="text-lg text-justify sm:text-left">Tanpa perlu repot memasukkan bahan ajar satu persatu, guru hanya perlu memberikan rencana pembelajaran. Bitlearn akan menyediakan ruang kelasnya. Pengajaran multi kelas menjadi lebih mudah dan tidak repot.</p>
           </div>
         </div>
 
@@ -63,17 +63,17 @@ const Index = ({ data, location }) => {
           <div className="grid grid-cols-2 md:grid-cols-6 gap-6">
             <div className="col-span-2 text-center p-8 space-y-4 border-2 border-black">
               <h3 className="md:text-3xl text-2xl text-center font-bold my-4" >Google Classroom</h3>
-              <img src={placeholder} className="block mx-auto" alt="placeholder"/>
+              <img src={placeholder} className="block text-justify mx-auto" alt="placeholder"/>
               <p>Ruang kelas utama anda. Disini anda dapat memindahkan seluruh kegiatan di kelas menjadi daring. Tidak perlu repot mengisi absen, semua akan terekam secara otomatis.</p>
             </div>
             <div className="col-span-2 text-center p-8 space-y-4 border-2 border-black">
               <h3 className="md:text-3xl text-2xl text-center font-bold my-4" >Google Drive</h3>
-              <img src={placeholder} className="block mx-auto" alt="placeholder"/>
+              <img src={placeholder} className="block text-justify mx-auto" alt="placeholder"/>
               <p>Perpustakaan untuk murid anda. Disini anda dapat memberikan modul, buku, bahkan video pembelajaran yang mampu meningkatkan keseruan belajar.</p>
             </div>
             <div className="col-span-2 text-center p-8 space-y-4 border-2 border-black">
               <h3 className="md:text-3xl text-2xl text-center font-bold my-4" >Google Meet</h3>
-              <img src={placeholder} className="block mx-auto" alt="placeholder"/>
+              <img src={placeholder} className="block text-justify mx-auto" alt="placeholder"/>
               <p>Pengajaran tatap muka sekarang bisa dilakukan lewat layar gawai anda. Pembelajaran dapat dilakukan secara sinkron seperti di ruang kelas konvensional.</p>
             </div>
           </div>
@@ -81,12 +81,12 @@ const Index = ({ data, location }) => {
             <div className="hidden sm:grid"></div>
             <div className="col-span-2 text-center p-8 space-y-4 border-2 border-black">
               <h3 className="md:text-3xl text-2xl text-center font-bold my-4" >Google Form</h3>
-              <img src={placeholder} className="block mx-auto" alt="placeholder"/>
+              <img src={placeholder} className="block text-justify mx-auto" alt="placeholder"/>
               <p>Tanpa menggunakan kertas, tanpa menggunakan pensil. Pengambilan nilai dapat anda lakukan menggunakan google form, tentunya secara aman dan bebas dari kecurangan.</p>
             </div>
             <div className="col-span-2 text-center p-8 space-y-4 border-2 border-black">
               <h3 className="md:text-3xl text-2xl text-center font-bold my-4" >Google Calendar</h3>
-              <img src={placeholder} className="block mx-auto" alt="placeholder"/>
+              <img src={placeholder} className="block text-justify mx-auto" alt="placeholder"/>
               <p>Jadwalkan seluruh kegiatan kelas secara terpadu. Siswa dapat memantau jadwal kelas,  batas akhir pengumpulan tugas, dan jadwal ujian tanpa repot.</p>
             </div>
             <div></div>
