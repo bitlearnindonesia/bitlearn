5a79ae82584fbbb1fa22e1852f051e57a63ed125
Ridwan Satria <73154922+riidwansatria@users.noreply.github.com>
2022-02-02 18:32:08 +0900

    header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import { Link } from 'gatsby'
 import { Disclosure } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
+import placeholder from '/src/images/placeholder.png'
+
 const navigation = [
   { name: 'Layanan', to: '#', current: false },
   { name: 'Tentang Kami', to: '#', current: false },
@@ -19,16 +21,17 @@ export default function Header() {
         {({ open }) => (
           <>
             <div className="max-w-6xl mx-auto px-2 sm:px-6 lg:px-8">
-              <div className="relative flex items-center justify-left h-24">
+              <div className="relative flex items-center justify-start h-24">
 
                 <div className="flex flex-1 sm:justify-start align-middle">
-                  <div className="flex flex-none">
+                  <div className="flex flex-1 items-center justify-center sm:justify-start">
+                    <img src={placeholder} className="w-8 h-8 mx-4" alt="placeholder"/>
                     <Link to="/">
-                      <p className="text-3xl font-bold inline">Bitlearn</p>
+                      <p className="text-3xl font-bold">Bitlearn</p>
                     </Link>
                   </div>
 
-                  <div className="flex flex-1 object-left">
+                  <div className="flex flex-none object-left">
                     <div className="hidden sm:flex sm:flex-grow sm:ml-6">
                       <div className="flex flex-none space-x-0">
                         {navigation.map((item) => (
@@ -50,8 +53,8 @@ export default function Header() {
                 </div>
 
                 <div className="flex-none sm:hidden">
-                  {/* Mobile menu button*/}
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-200">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-800 focus:outline-none">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -64,14 +67,14 @@ export default function Header() {
             </div>
 
             <Disclosure.Panel className="sm:hidden">
-              <div className="px-2 pt-2 pb-3 space-y-1">
+              <div className="border-t-2 border-black">
                 {navigation.map((item) => (
                   <Link
                     key={item.name}
                     to={item.to}
                     className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'flex text-gray-800 hover:bg-gray-200',
-                      'inline px-3 py-2 rounded-md align-center text-base font-medium'
+                      item.current ? 'bg-gray-900 text-white' : 'text-gray-800 hover:bg-gray-200',
+                      'flex px-3 py-2 justify-center text-base font-bold border-b-2 border-black'
                     )}
                     aria-current={item.current ? 'page' : undefined}
                   >
