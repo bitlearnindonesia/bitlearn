f96d798f4d62ef44c93605a12446fe07dcb7faf4
Figo Alfaritzi <figoalfaritzi@gmail.com>
2022-03-31 19:41:43 +1000

    Change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
             <ul>
               <li>M. Wierdy Bagus Nugrahaien</li>
               <li>+62 878 8190 3463</li>
-              <li>wierdybagus@gmail.com</li>
+              <li>cs@bitlearn.id</li>
             </ul>
           </div>
             <div className="col-span-3">
@@ -44,12 +44,12 @@ const Footer = () => {
           </ul>
           <img src={logo} className="col-span-2 w-36 sm:w-48 py-12 mx-auto sm:ml-0" alt="placeholder"/>
           <div className='col-span-2 sm:col-span-4' id='about'>
-            <div className="sm:hidden text-xl text-center font-bold py-4">Tentang Bitlearn</div>
+            <div className="sm text-xl text-center font-bold py-4">PT. Abudi Cipta Kreasi</div>
             <div className="text-lg text-justify">Kami memiliki tujuan dasar untuk membantu pendidikan di Indonesia menjadi lebih baik. Menggunakan teknologi, kami berharap dapat membantu guru untuk memberikan pelajaran dengan lebih mudah agar murid dapat menyerap ilmu dengan yang diajarkan lebih sempurna.</div>
           </div>
         </main>
       </div>
-      <div className="bg-primary text-white text-center text-sm py-2">© {new Date().getFullYear()} Tim Bitlearn. All Rights Reserved</div>
+      <div className="bg-primary text-white text-center text-sm py-2">© {new Date().getFullYear()} PT. Abudi Cipta Kreasi. All Rights Reserved</div>
     </body>
   )
 }
